4e042e90978283b66a1ffc0c6f411c87c90ffadd
Kudah Shambare <Kudahshambare263@gmail.com>
2021-03-03 23:08:57 +0200

    Shopping List Done

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,6 +1,23 @@
 function shoppingList(arrayOfPeople) {
   // Write your code here...
+  let content = document.getElementById("content");
+  let unorderedList = document.createElement("ul");
+  content.appendChild(unorderedList);
+
+  for (let i = 0; i < arrayOfPeople.length; i++) {
+    function makeIt() {
+      let listItem = document.createElement("li");
+      listItem.className = "shoppingList";
+      listItem.innerHTML = arrayOfPeople[i];
+
+      unorderedList.appendChild(listItem);
+    }
+
+    makeIt();
+  }
 }
+
+console.dir("content");
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
 
