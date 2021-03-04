3f3041a27ddf07b8a18ad3719084d2e0fcfc3b0f
Kudah Shambare <Kudahshambare263@gmail.com>
2021-03-04 18:10:04 +0200

    ToDo List Done

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,28 +1,26 @@
 function todoList(todos) {
   // Write your code here...
-  let content=document.getElementById("content");
-  let unorderedList=document.createElement("ul");
+  let content = document.getElementById("content");
+  let unorderedList = document.createElement("ul");
   content.appendChild(unorderedList);
-  for(let i=0;i<todos.length;i++){
-    function createItem(){
-      let listItem=document.createElement("li");
-      listItem.innerHTML=todos[i].todo;
+  for (let i = 0; i < todos.length; i++) {
+    function createItem() {
+      let listItem = document.createElement("li");
+      listItem.innerHTML = todos[i].todo;
+      listItem.addEventListener("click", function () {
+        listItem.style.textDecoration = "line-through";
+      });
       unorderedList.appendChild(listItem);
     }
     createItem();
   }
- /*ADD Events*///////////////////////////////////////////////////////////////////////////////////
-
-
 }
- 
-
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
